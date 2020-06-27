3d12a3b49a110a0e08fb0dabf3a795cd03dae06f
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-27 10:57:18 +0530

    rename location to choose_location

---
+++ lib/screens/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocationScreen extends StatefulWidget {
+  @override
+  _ChooseLocationScreenState createState() => _ChooseLocationScreenState();
+}
+
+class _ChooseLocationScreenState extends State<ChooseLocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('choose location screen'),
+    );
+  }
+}

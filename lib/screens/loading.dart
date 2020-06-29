4b09bb96f5e2f6b7e9b61a2f073dbd6b53a0d199
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-29 08:27:29 +0530

    update loading screen

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,30 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  setupWorldTime() async {
+    WorldTimeService _worldTime = WorldTimeService(
+        location: 'USA',
+        flag: 'assets/images/usa.png',
+        url: 'America/New_York');
+    await _worldTime.getTime();
+    Navigator.pushReplacementNamed(
+      context,
+      '/home',
+      arguments: {
+        'location': _worldTime.location,
+        'flag': _worldTime.flag,
+        'time': _worldTime.time,
+        'isDayTime': _worldTime.isDayTime,
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
